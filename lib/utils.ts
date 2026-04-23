e320d3f51e83e4d6a417f0e0fba5c71356d55eb0
melevgeni-cpu <mel.evgeni@gmail.com>
2026-04-23 20:12:05 +0300

    Update utils.ts
    В lib/utils.ts используется пакет clsx, которого нет в вашем package.json.
    На самом деле функция cn (className-хелпер) нигде не нужна в проекте, поэтому её удалил, чтобы не добавлять лишние зависимости.

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,10 +1,3 @@
-import { type ClassValue, clsx } from 'clsx'
-import { twMerge } from 'tailwind-merge'
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
-
 export function formatPhoneNumber(phone: string): string {
   const cleaned = phone.replace(/\D/g, '')
   const match = cleaned.match(/^(\d{1})(\d{3})(\d{3})(\d{2})(\d{2})$/)
